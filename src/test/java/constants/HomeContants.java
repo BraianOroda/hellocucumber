5ea1238f75f06e35413ce5ed87cdb04bfb2832f2
EXB83882 <boroda@vates.com>
2023-12-09 03:18:47 -0300

    Create HomeContants.java

---
+++ src/test/java/constants/HomeContants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class HomeContants {
+    public static final String INPUT_USER_NAME = "//input[@data-test='username']";
+    public static final String INPUT_PASSWORD = "//input[@data-test='password']";
+    public static final String LOGGIN_BUTTOM = "//input[@id='login-button']";
+}
