d14d917087324cadf34b77f218f480971c17375f
EXB83882 <boroda@vates.com>
2023-12-09 03:18:38 -0300

    Update StepDefinitions.java

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -1,6 +1,5 @@
 package hellocucumber;
 
-import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -9,24 +8,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.html5.SessionStorage;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageobject.HomePage;
+
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Friday".equals(today) ? "TGIF" : "Nope";
-    }
-}
-
-public class StepDefinitions {
+public class StepDefinitions{
     WebDriver driver;
-    private String today;
-    private String actualAnswer;
-
+    HomePage homePage;
 
     public void startChromeDriver() {
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
@@ -35,97 +27,38 @@ public class StepDefinitions {
         driver.manage().window().maximize();
     }
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
-    }
-
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
-    }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-    }
-
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
-    }
-
-    @Given("today is {string}")
-    public void today_is(String today) {
-        this.today = today;
-    }
-
-    @When("I ask whether it's Friday yet")
-    public void i_ask_whether_it_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
-    }
-
-    @Then("I should be told {string}")
-    public void i_should_be_told(String expectedAnswer) {
-        assertEquals(expectedAnswer, actualAnswer);
-    }
-
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
-    }
-
-    ///////////////////////////////////////////////////////////////
-
-    @Given("I am on the Google search page")
-    public void I_visit_google() {
-        //startChromeDriver();
-    }
-
-    /*
-    @When("I search for {string}")
-    public void search_for(String query) {
-        WebElement element = driver.findElement(By.name("q"));
-        // Enter something to search for
-        element.sendKeys(query);
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-    }
-
-    @Then("the page title should start with {string}")
-    public void checkTitle(String titleStartsWith) {
-        // Google's search is rendered dynamically with JavaScript
-        // Wait for the page to load timeout after ten seconds
-        new WebDriverWait(driver,10L).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith(titleStartsWith);
-            }
-        });
-    }
-
-    @After()
-    public void closeBrowser() {
-        driver.quit();
-    }
-
-     */
-
-    ///////////////////////////////////////////////////////
-
     @Given("The user do login on the web")
     public void theUserDoLoginOnTheWeb() {
         startChromeDriver();
     }
 
+    /*
     @When("The user put the user name and password")
     public void theUserPutTheUserNameAndPassword() {
-        WebElement a = driver.findElement(By.xpath("//input[@data-test='username']"));
-        a.sendKeys("dfsaffadfaq");
+        WebElement inputUserName = driver.findElement(By.xpath("//input[@data-test='username']"));
+        inputUserName.sendKeys("standard_user");
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@data-test='password']"));
+        inputPassword.sendKeys("secret_sauce");
+        WebElement logginButton = driver.findElement(By.xpath("//input[@id='login-button']"));
+        logginButton.click();
+        Wait<WebDriver> wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOf(inputUserName));
+    }
+    */
+
+    @When("The user put the user name and password")
+    public void theUserPutTheUserNameAndPassword() {
+        homePage.loginUser();
     }
 
     @Then("The user will see the items")
     public void theUserWillSeeTheItems() {
-
+        List<WebElement> listaItems = driver.findElements(By.xpath("//div[@class='inventory_item']"));
+        assert listaItems.size() > 0;
     }
 
     @And("The user do logout")
     public void theUserDoLogout() {
+        driver.quit();
     }
 }
