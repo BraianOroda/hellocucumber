0c7cf2a4805479c578fe1668899600ea57dc522a
EXB83882 <boroda@vates.com>
2024-05-16 17:38:02 -0300

    Update CucumberHooks.java

--- src/test/java/hellocucumber/steps/CucumberHooks.java
+++ src/test/java/hellocucumber/steps/CucumberHooks.java
@@ -2,17 +2,30 @@ package hellocucumber.steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CucumberHooks {
     public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
     @Before
     public void startChromeDriver() {
+        /*Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "Nexus 5");*/
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+//        options.setExperimentalOption("mobileEmulation", mobileEmulation);
+//        options.addArguments("--enable-logging=stderr");
+//        options.addArguments("-enable-logging=stderr --v=2");
+//        options.addArguments("--headless");
         System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
-        tlDriver.set(new ChromeDriver());
-        getDriver().manage().window().maximize();
+        tlDriver.set(new ChromeDriver(options));
+//        tlDriver.set(new ChromeDriver());
+//        getDriver().manage().window().maximize();
         getDriver().get("https://www.saucedemo.com/v1/");
     }
     public static synchronized WebDriver getDriver() {
